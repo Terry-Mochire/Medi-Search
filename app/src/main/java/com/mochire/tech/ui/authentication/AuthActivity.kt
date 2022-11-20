92cbcc4c353cfe1a1e894d15ff04ac68dd46f950
Terry <terrybmochire@gmail.com>
2022-11-20 21:15:17 +0300

    Implement Google Sign In.

--- app/src/main/java/com/mochire/tech/ui/authentication/AuthActivity.kt
+++ app/src/main/java/com/mochire/tech/ui/authentication/AuthActivity.kt
@@ -1,8 +1,14 @@
 package com.mochire.tech.ui.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.mochire.tech.MainActivity
 import com.mochire.tech.R
+
 
 @Suppress("DEPRECATION")
 class AuthActivity: AppCompatActivity() {
@@ -14,5 +20,42 @@ class AuthActivity: AppCompatActivity() {
             android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        val googleSignInButton = findViewById<ImageView>(R.id.googleSignIn)!!
+        googleSignInButton.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 1)
+        }
+
+
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(com.google.android.gms.common.api.ApiException::class.java)!!
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
