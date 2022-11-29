04f7679e6eb371d62249dd3d99954b56d79cd959
Terry <terrybmochire@gmail.com>
2022-11-30 00:26:55 +0300

    Add Home fragment UI

--- app/src/main/java/com/mochire/tech/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/mochire/tech/ui/main/home/HomeFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.mochire.tech.R
 import com.mochire.tech.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,10 +24,24 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        Glide.with(this)
+            .load(R.drawable.search)
+            .into(binding.gif)
+
+
+        binding.startSymptomAssessment.setOnClickListener {
+            val fragment = AssessmentFragment()
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment_activity_main2, fragment)
+            transaction.addToBackStack(this.javaClass.name)
+            transaction.commit()
+
+        }
 
         return root
     }
