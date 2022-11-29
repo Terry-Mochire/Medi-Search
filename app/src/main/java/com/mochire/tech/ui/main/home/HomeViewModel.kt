dd538dba3f13ee80d377c6b685a8d4dacb869cb9
Terry <terrybmochire@gmail.com>
2022-11-30 00:27:37 +0300

    Implement method to collect all symptoms

--- app/src/main/java/com/mochire/tech/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/com/mochire/tech/ui/main/home/HomeViewModel.kt
@@ -1,10 +1,18 @@
 package com.mochire.tech.ui.main.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mochire.tech.api.ApiClient
 
 class HomeViewModel : ViewModel() {
 
+        private val api = ApiClient().getApi()
+        val symptomName_symptomId = HashMap<String, String>()
 
+        suspend fun loadSymptoms(): HashMap<String, String> {
+            val response = api.getSymptoms(25)
+            for (symptom in response) {
+                symptomName_symptomId[symptom.name] = symptom.id
+            }
+            return symptomName_symptomId
+        }
 }
